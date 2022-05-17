fac9d9639c15430bc488880bd06062db1db6b324
João Vitor <leof.neres@gmail.com>
2022-05-17 15:29:43 -0300

    web routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('customers')->group(function () {
+    Route::get('/', [App\Http\Controllers\CustomerController::class, 'index'])->name('customers.index');
+    Route::get('/create', [App\Http\Controllers\CustomerController::class, 'create'])->name('customers.create');
+    Route::post('/store', [App\Http\Controllers\CustomerController::class, 'store'])->name('customers.store');
+    Route::get('/{id}/show', [App\Http\Controllers\CustomerController::class, 'show'])->name('customers.show');
+    Route::get('/{id}/edit', [App\Http\Controllers\CustomerController::class, 'edit'])->name('customers.edit');
+    Route::put('/{id}/update', [App\Http\Controllers\CustomerController::class, 'update'])->name('customers.update');
+    Route::delete('/{id}/delete', [App\Http\Controllers\CustomerController::class, 'delete'])->name('customers.delete');
+});
