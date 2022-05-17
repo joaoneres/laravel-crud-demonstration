b5b5e15c1072648cbb5e8e66279cdce922c22d66
João Vitor <leof.neres@gmail.com>
2022-05-17 15:30:53 -0300

    customer factory

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'phone' => $this->faker->phoneNumber(),
+            'email' => $this->faker->safeEmail(),
+            'birth' => $this->faker->date('Y-m-d'),
+        ];
+    }
+}
