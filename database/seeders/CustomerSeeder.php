21bec6a5c9dd0df191eb81a857b6cbbcf02fc320
João Vitor <leof.neres@gmail.com>
2022-05-17 15:30:39 -0300

    seeder

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CustomerSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            CustomerSeeder::class,
-        ]);
+        Customer::factory()->times(15)->create();
     }
 }
